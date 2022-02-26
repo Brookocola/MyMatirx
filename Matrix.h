17a71c2a4815c06a06af27d4fcd1775dafa6c414
Brookocola <2015564688@qq.com>
2022-02-26 10:52:56 +0800

    Modify .h

--- Matrix.h
+++ Matrix.h
@@ -22,7 +22,13 @@ public:
 	T Det() const;//矩阵求行列式
 	int Rank() const;//求矩阵的秩
 	Matrix<T> Rref() const;//求简化的行阶梯形矩阵
-
+	void Delete_row(int row);//删除矩阵某行
+	void Delete_col(int col);//删除矩阵某列
+	Matrix<T> Delete_row_col(int row, int col) const;//删除矩阵某行某列
+	template<class Type> 
+	friend Type Cofactor(int i, int j, Matrix<Type> M);//求代数余子式
+	template<class Type>
+	friend Type Det(Matrix<Type> M);
 
 	//操作符重载
 	T& operator ()(int row, int col);//()操作符重载,用于访问矩阵元素
@@ -125,6 +131,102 @@ Matrix<T> Matrix<T>::Transpose() const {
 	}
 	return tmp;
 }
+template<class T>
+T Matrix<T>::Det() const {
+	if (this->size == 1) {
+		return this->data[0];
+	}
+	else {
+		T det = 0;
+		//按第一行展开
+		for (int j = 0; j < this->cols; j++) {
+			Matrix<T> tmp = this->Delete_row_col(1, j + 1);
+			det +=this->data[0 * this->cols + j] * tmp.Det()*(j % 2 ? -1 : 1);
+		}
+		return det;
+	}
+}
+template<class T>
+void Matrix<T>::Delete_row(int row) {
+	if (row<1 || row>this->rows) {
+		return;//超过矩阵范围
+	}
+	else {
+		Matrix<T> tmp=*this;
+		delete[] data;
+		this->rows = tmp.rows - 1;
+		this->size = this->rows*this->cols;
+		data = new T[this->size];
+		row--;
+		for (int i = 0; i < this->size; i++) {
+			if (i < row * this->cols) {
+				this->data[i] = tmp.data[i];
+			}
+			else {
+				this->data[i] = tmp.data[i + this->cols];
+			}
+		}
+	}
+}
+template<class T>
+void Matrix<T>::Delete_col(int col) {
+	if (col<1 || col>this->cols) {
+		return;//超过矩阵范围
+	}
+	else {
+		Matrix<T> tmp = *this;
+		delete[] data;
+		this->cols = tmp.cols - 1;
+		this->size = this->rows*this->cols;
+		data = new T[this->size];
+		col--;
+		int flag = 0;
+		for (int i = 0; i < this->size; i++) {
+			if (col == this->cols) {
+				if (i != 0 && i%this->cols == 0) {
+					flag++;
+				}
+			}
+			else {
+				if (i%this->cols == col) {
+					flag++;
+				}
+			}
+			
+			this->data[i] = tmp.data[i + flag];
+		}
+	}
+}
+template<class T>
+Matrix<T> Matrix<T>::Delete_row_col(int row, int col) const {
+	Matrix<T> tmp = *this;
+	tmp.Delete_row(row);
+	tmp.Delete_col(col);
+	return tmp;
+}
+template<class Type>
+Type Cofactor(int i, int j, Matrix<Type> M) {
+	Matrix<Type> tmp = M;
+	tmp.Delete_row(i);
+	tmp.Delete_col(j);
+	return ((i+j) % 2 ? -1 : 1)*Det(tmp);
+}
+template<class Type>
+Type Det(Matrix<Type> M) {
+	if (M.size== 1) {
+		return M.data[0];
+	}
+	else {
+		Type det = 0;
+		//按第一行展开
+		for (int j = 0; j < M.cols; j++) {
+			Matrix<Type> tmp=M.Delete_row_col(1, j + 1);
+			det += M.data[0 * M.cols + j]*Det(tmp)*(j % 2 ? -1 : 1);
+		}
+		return det;
+	}
+}
+//操作符重载函数
 template<class T>
 T& Matrix<T>::operator()(int row, int col) {
 	row--;//矩阵索引从1开始
--- MyMatrix.cpp
+++ MyMatrix.cpp
@@ -6,14 +6,30 @@ using namespace std;
 
 int main()
 {
-	Matrix<int> a(3,4,1);
+	/*Matrix<int> a(5,5,1);
 	a(1, 2) = 5;
+	a(2, 2) = 6;
+	a(3, 2) = 7;
+	a(4, 2) = 8;
+	a(5, 2) = 9;
+	a(5, 3) = 9;
 	a.print();
-	Matrix<double> c(2, 2, 3.2);
+	a.Delete_col(3);
+	a.Delete_row(2);
+	a.print();*/
+
+	Matrix<int> a(2, 2, 1);
+	a(2, 2) = 3;
+	a(2, 1) = 7;
+	a.print();
+	cout << Cofactor(1, 2, a);
+	//cout <<a.Det();
+
+	/*Matrix<double> c(2, 2, 3.2);
 	Matrix<double> d(2, 2, 2.1);
 	d(1, 1) = 9;
 	Matrix<double> f = d/2.0;
-	f.print();
+	f.print();*/
 	/*Matrix<int> b=(a*3);
 	b.print();
 	(a + b).print();*/
